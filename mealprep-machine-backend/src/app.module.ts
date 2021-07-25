39177e8a9ddd30310b410b8919cb86dc7f7fbf71
Jeffrey <test@example.com>
2021-07-24 21:59:06 -0500

    added initial sqlite config

--- mealprep-machine-backend/src/app.module.ts
+++ mealprep-machine-backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.db',
+      synchronize: true,
+      logging: false,
+      autoLoadEntities: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
