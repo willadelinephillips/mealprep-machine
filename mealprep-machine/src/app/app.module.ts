d736771ced34b0c28bbe8e837cba983c39df69bb
Will <willadelinephillips@gmail.com>
2021-07-28 12:00:30 -0700

    Just.... dumped all the material modules in one place

--- mealprep-machine/src/app/app.module.ts
+++ mealprep-machine/src/app/app.module.ts
@@ -1,11 +1,4 @@
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MaterialModule } from './material.module';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -19,14 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent
   ],
   imports: [
-    MatToolbarModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatInputModule,
-    MatIconModule,
-    MatDividerModule,
-    MatListModule,
-    MatCheckboxModule,
+    MaterialModule,
 
     BrowserModule,
     AppRoutingModule,
